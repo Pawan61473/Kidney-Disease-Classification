0b250451a3b2e141bb191fbe8b6fc180019945eb
Pawan61473 <pawanbharat7@gmail.com>
2025-01-15 18:22:03 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Kidney_Disease_Classification import logger
 from Kidney_Disease_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from Kidney_Disease_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from Kidney_Disease_Classification.entity.config_entity import(DataIngestionConfig,PrepareBaseModelConfig)  
 
 
 
@@ -15,3 +16,14 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
